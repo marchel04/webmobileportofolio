8c365c32fbe6163d91ec26575d944604d67611fb
marchel04 <144196935+marchel04@users.noreply.github.com>
2025-06-26 14:21:08 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,35 @@
-const cacheName = 'portfolio-cache-v1';
-const assetsToCache = [
-  '/index.html',
-  '/project.html',
-  '/manifest.json',
-  '/style.css',
-  '/images/saya.jpg',
-  '/images/aws.png',
-  '/images/Pemrograman_Komputer.png',
-  '/images/html.jpg',
-  '/images/css.jpg',
-  '/images/java.jpg'
+const cacheName = 'portfolio-v1';
+const assets = [
+  './',
+  './index.html',
+  './project.html',
+  './style.css',
+  './manifest.json',
+  './images/saya.jpg',
+  './images/aws.png',
+  './images/Pemrograman_Komputer.png',
+  './images/html.jpg',
+  './images/css.jpg',
+  './images/java.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll(assetsToCache))
-      .catch(err => console.error('❌ Gagal cache:', err))
+      .then(cache => {
+        return cache.addAll(assets);
+      })
+      .then(() => console.log('✅ Semua file berhasil dicache.'))
+      .catch(error => {
+        console.error('❌ Gagal mencache:', error);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
   );
 });
